00eb632c0059aef9d19feef7b3b5ae42ad0fd333
winterfroststrom <winterfroststorm@gmail.com>
2013-12-09 06:10:06 -0500

    added non-greedy planner

--- song_planner.js
+++ song_planner.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var song1 = eval(fs.readFileSync("songarrayFurElise2")+'');
 //var song1 = eval(fs.readFileSync("songarrayRamble")+'');
 
+
 function collide(arm1, arm2, note1, note2){
 	if(arm1 == arm2) {
 		return true;
@@ -92,6 +93,45 @@ var Queue = function (){
 	};
 }
 
+var SortedBuffer = function(size){
+	var items = [];
+	var costs = [];
+
+	this.push = function(item, cost){
+		if(items.length == 0){
+			items.push(item);
+			costs.push(cost);
+		} else if(cost < costs[costs.length - 1]){
+			if(items.length + 1 > size){
+				items.pop();
+				costs.pop();	
+			}
+			items.push(item);
+			costs.push(cost);
+			var index = costs.length - 1;
+			while(costs[index] < costs[index - 1]){
+				items[index] = items[index - 1];
+				costs[index] = costs[index - 1];
+				items[index - 1] = item;
+				costs[index - 1] = cost;
+				index--;
+			}
+		}
+	};
+
+	this.get = function (index){
+		return [items[index], costs[index]];
+	};
+	
+	this.toArray = function(){
+		var output = [];
+		for(var i = 0; i < costs.length; i++){
+			output[i] = this.get(i);
+		}
+		return output;
+	};
+}
+
 function containsAll(arr, test){
 	for(var i = 0; i < test.length; i++){
 		if(arr.indexOf(test[i]) == -1){
@@ -126,7 +166,7 @@ function minCostNextState(oldPositions, songData, songIndex){
 	var minNote = 36;
 	var maxNote = 83;
 	var notes = playingNotes(songData.slice(0, 4));
-	var minDistance = Math.pow(48, 4);
+	var minDistance = Math.pow(maxNote - minNote, 4);
 	var minPositions = [-1, -1, -1, -1];
 	var minVelocities = [-1, -1, -1, -1];
 	for(var i = minNote; i <= maxNote; i++){
@@ -149,38 +189,103 @@ function minCostNextState(oldPositions, songData, songIndex){
 	return minPositions.concat(minVelocities).concat([songData[4]]).concat([songIndex]);
 }
 
-function dfsAstar(song, arm_positions){
-	var ds = [];
-	ds.push([arm_positions[0], arm_positions[1], arm_positions[2], arm_positions[3], 0, 0, 0, 0, 0, 0]);
-	var visited = [];
-	while(ds.length > 0){
-		var current = ds.pop();
-		var songIndex = current[9];
-		if(typeof visited[songIndex] == 'undefined'){
-			visited[songIndex] = current.slice(0, 9);
+function greedyPlanner(song, arm_positions){
+	var plan = [];
+	plan.push([arm_positions[0], arm_positions[1], arm_positions[2], arm_positions[3], 0, 0, 0, 0, 0]);
+	for(var i = 0; i < song.length; i++){
+		var current = plan[plan.length - 1];
+		var next = minCostNextState(current.slice(0, 4), song[i], i + 1);
+		if(next[0] != -1){
+			plan.push(next.slice(0, 9));
 		} else {
-			continue;
+//			console.log("Unplayable note " + song[i][4]+ " at index " + i + "!");
 		}
-		if(visited.length >= song.length){
-			return visited;
+	}
+	return plan;
+}
+
+function multiMinCostNextState(oldPositions, songData, songIndex, limit){
+	var minNote = 36;
+	var maxNote = 83;
+	var notes = playingNotes(songData.slice(0, 4));
+	var buffer = new SortedBuffer(limit);
+	var commonData = [songData[4], songIndex];
+	for(var i = minNote; i <= maxNote; i++){
+		for(var j = i + 1; j < maxNote; j++){
+			for(var k = j + 1; k < maxNote; k++){
+				for(var l = k + 1; l < maxNote; l++){
+					var possibility = [i, j, k, l];
+					if(containsAll(possibility, notes) && !anyCollide(possibility)){
+						var distance = l2Distance2(possibility, oldPositions);
+						var item = possibility.concat(velocities(possibility, notes)).concat(commonData);
+						buffer.push(item, distance);
+					}
+				}
+			}
 		}
-		var next = minCostNextState(current.slice(0, 4), song[songIndex], songIndex + 1);
-		ds.push(next);
+	}
+	return buffer.toArray();
+}
+
+function limitedSearch(song, arm_positions, startIndex, limit, branching){
+	var ds = [];
+	ds.push([[arm_positions[0], arm_positions[1], arm_positions[2], arm_positions[3], 0, 0, 0, 0, 0, startIndex, 0]]);
+	var progress = 0;
+	while(ds.length > 0){
+		var currentCost = 999999999999;
+		var currentIndex = -1;
+		for(var i = 0; i < ds.length;i++){
+			var possibleCost = 0;
+			for(var j = 0; j < ds[i].length;j++){
+				possibleCost += ds[i][j][10];
+			}
+			if(possibleCost < currentCost){
+				currentIndex = i;
+				currentCost = possibleCost;
+			}
+		}
+		var current = ds.splice(currentIndex, 1)[0];
+		var songIndex = current[current.length - 1][9];
+		progress = Math.max(progress, songIndex - startIndex);
+		if(progress >= limit){
+			return current;
+		}
+		var nexts = multiMinCostNextState(current[current.length - 1].slice(0, 4), song[songIndex], songIndex + 1, branching);
+		for(var i = 0; i < nexts.length;i++){
+			var next = current.slice(0);
+			next.push(nexts[i][0].concat(nexts[i][1]));
+			ds.push(next);
+		}
 	}
 	return null;
 }
 
-function onlineDfsAstar(songData, arm_positions){
+function limitedSearchPlanner(song, arm_positions){
+	var plan = [[arm_positions[0], arm_positions[1], arm_positions[2], arm_positions[3], 0, 0, 0, 0, 0, 0]];
+	var searchDepth = 2;
+	var branching = 5;
+	for(var i = 0; i < song.length; i++){
+		var next = limitedSearch(song, plan[plan.length - 1].slice(0, 4), i, Math.min(searchDepth, song.length - i), branching);
+		if(next != null){
+			next = next[1].slice(0, 9);
+			plan.push(next);
+		}
+	}
+	return plan;
+}
+
+function onlineGreedyPlanner(songData, arm_positions){
 	return minCostNextState(arm_positions.slice(0, 4), songData, -1).slice(0, 9);
 }
 
-console.log(dfsAstar(song1, [68, 70, 74, 76]));
-//console.log(onlineDfsAstar([0, 0, 0, 0, 350], [68, 70, 74, 76]));
+function onlineLimitedSearchPlanner(songData, arm_positions){
+	var searchDepth = 2;
+	var branching = 5;
+	return limitedSearch(song, arm_positions.slice(0, 4), songData, Math.min(searchDepth, song.length - i), branching);
+}
 
-
-
-
-
+//console.log(greedyPlanner(song1, [68, 70, 74, 76]));
+console.log(limitedSearchPlanner(song1, [68, 70, 74, 76]));
 
 
 
