3bceb3ab76435a80fa006201506706c221b11aa0
winterfroststrom <winterfroststorm@gmail.com>
2013-12-09 19:01:02 -0500

    backup point

--- song_planner.js
+++ song_planner.js
@@ -1,15 +1,22 @@
 var fs = require('fs');
 
+function loadSong(file){
+	return eval(fs.readFileSync(file)+'');
+}
+
 //var song1 = eval(fs.readFileSync("songarrayFurElise")+'');
-var song1 = eval(fs.readFileSync("songarrayFurElise2")+'');
+//var song1 = eval(fs.readFileSync("songarrayFurElise2")+'');
+//var song1 = eval(fs.readFileSync("songarray_elise", 'utf8'));
+//var song1 = loadSong("elise_first.txt");
+//var song1 = loadSong("ramblinArray.txt");
 //var song1 = eval(fs.readFileSync("songarray2FurElise2")+'');
 //var song1 = eval(fs.readFileSync("songarrayRamble")+'');
 //var song1 = eval(fs.readFileSync("tt")+'');
-
+var song1 = loadSong("first.txt");
 
 var minNote = 48;
 var maxNote = 95;
-var searchDepth = 3;
+var searchDepth = 2;
 var branching = 5;
 
 function collide(arm1, arm2, note1, note2){
@@ -130,6 +137,10 @@ var SortedBuffer = function(size){
 		return [items[index], costs[index]];
 	};
 	
+	this.length = function(){
+		return items.length;
+	};
+	
 	this.toArray = function(){
 		var output = [];
 		for(var i = 0; i < costs.length; i++){
@@ -174,10 +185,10 @@ function minCostNextState(oldPositions, songData, songIndex){
 	var minDistance = Math.pow(maxNote - minNote, 4);
 	var minPositions = [-1, -1, -1, -1];
 	var minVelocities = [-1, -1, -1, -1];
-	for(var i = minNote; i <= maxNote; i++){
+	for(var i = minNote; i < maxNote; i++){
 		for(var j = i + 1; j < maxNote; j++){
 			for(var k = j + 1; k < maxNote; k++){
-				for(var l = k + 1; l < maxNote; l++){
+				for(var l = k + 1; l <= maxNote; l++){
 					var possibility = [i, j, k, l];
 					if(containsAll(possibility, notes) && !anyCollide(possibility)){
 						var distance = l2Distance2(possibility, oldPositions);
@@ -191,12 +202,17 @@ function minCostNextState(oldPositions, songData, songIndex){
 			}
 		}
 	}
-	return minPositions.concat(minVelocities).concat([songData[4]]).concat([songIndex]);
+	if(minDistance == Math.pow(maxNote - minNote, 4)){
+		return oldPositions.concat([0, 0, 0, 0]).concat([songData[4]]).concat([songIndex]);
+	} else {
+		return minPositions.concat(minVelocities).concat([songData[4]]).concat([songIndex]);
+	}
 }
 
 function greedyPlanner(song, arm_positions){
 	var plan = [];
 	plan.push([arm_positions[0], arm_positions[1], arm_positions[2], arm_positions[3], 0, 0, 0, 0, 0]);
+	var t = new Date();
 	for(var i = 0; i < song.length; i++){
 		var current = plan[plan.length - 1];
 		var next = minCostNextState(current.slice(0, 4), song[i], i + 1);
@@ -206,6 +222,7 @@ function greedyPlanner(song, arm_positions){
 //			console.log("Unplayable note " + song[i]+ " at index " + i + "!");
 		}
 	}
+//	console.log((new Date()) - t);
 	return plan;
 }
 
@@ -213,10 +230,10 @@ function multiMinCostNextState(oldPositions, songData, songIndex, limit){
 	var notes = playingNotes(songData.slice(0, 4));
 	var buffer = new SortedBuffer(limit);
 	var commonData = [songData[4], songIndex];
-	for(var i = minNote; i <= maxNote; i++){
+	for(var i = minNote; i < maxNote; i++){
 		for(var j = i + 1; j < maxNote; j++){
 			for(var k = j + 1; k < maxNote; k++){
-				for(var l = k + 1; l < maxNote; l++){
+				for(var l = k + 1; l <= maxNote; l++){
 					var possibility = [i, j, k, l];
 					if(containsAll(possibility, notes) && !anyCollide(possibility)){
 						var distance = l2Distance2(possibility, oldPositions);
@@ -227,7 +244,98 @@ function multiMinCostNextState(oldPositions, songData, songIndex, limit){
 			}
 		}
 	}
-	return buffer.toArray();
+	if(buffer.length() == 0){
+		buffer.push(oldPositions.concat([0, 0, 0, 0]).concat(commonData), 0);
+		return buffer.toArray();		
+	} else {
+		return buffer.toArray();
+	}
+}
+
+function betterMultiMinCostNextState(oldPositions, songData, songIndex, limit){
+	var notes = playingNotes(songData.slice(0, 4));
+	var buffer = new SortedBuffer(limit);
+	var commonData = [songData[4], songIndex];
+	for(var i = 0; i < notes.length;i++){
+		if(notes[i] < minNote || notes[i] > maxNote){
+			buffer.push(oldPositions.concat([0, 0, 0, 0]).concat(commonData), 0);
+			return buffer.toArray();
+		}
+	}
+	switch(notes.length){
+		case 4: 
+			var possibility = notes;
+			if(!anyCollide(possibility)){
+				var distance = l2Distance2(possibility, oldPositions);
+				var item = possibility.concat(velocities(possibility, notes)).concat(commonData);
+				buffer.push(item, distance);
+			}
+			break;
+		case 3: 
+			for(var i = minNote; i <= maxNote; i++){
+				if(notes.indexOf(i) != -1) continue;
+				var possibility = [i, notes[0], notes[1], notes[2]].sort();
+				if(!anyCollide(possibility)){
+					var distance = l2Distance2(possibility, oldPositions);
+					var item = possibility.concat(velocities(possibility, notes)).concat(commonData);
+					buffer.push(item, distance);
+				}
+			}
+			break;
+		case 2:
+			for(var i = minNote; i < maxNote; i++){
+				if(notes.indexOf(i) != -1) continue;
+				for(var j = i + 1; j <= maxNote; j++){
+					if(notes.indexOf(j) != -1) continue;
+					var possibility = [i, j, notes[0], notes[1]].sort();
+					if(!anyCollide(possibility)){
+						var distance = l2Distance2(possibility, oldPositions);
+						var item = possibility.concat(velocities(possibility, notes)).concat(commonData);
+						buffer.push(item, distance);
+					}
+				}
+			}
+			break;
+		case 1:
+			for(var i = minNote; i < maxNote; i++){
+				if(notes.indexOf(i) != -1) continue;
+				for(var j = i + 1; j < maxNote; j++){
+					if(notes.indexOf(j) != -1) continue;
+					for(var k = j + 1; k <= maxNote; k++){
+						if(notes.indexOf(k) != -1) continue;
+						var possibility = [i, j, k, notes[0]].sort();
+						if(!anyCollide(possibility)){
+							var distance = l2Distance2(possibility, oldPositions);
+							var item = possibility.concat(velocities(possibility, notes)).concat(commonData);
+							buffer.push(item, distance);
+						}
+					}
+				}
+			}
+			break;
+		default:
+			for(var i = minNote; i < maxNote; i++){
+				for(var j = i + 1; j < maxNote; j++){
+					for(var k = j + 1; k < maxNote; k++){
+						for(var l = k + 1; l <= maxNote; l++){
+							var possibility = [i, j, k, l];
+							if(containsAll(possibility, notes) && !anyCollide(possibility)){
+								var distance = l2Distance2(possibility, oldPositions);
+								var item = possibility.concat(velocities(possibility, notes)).concat(commonData);
+								buffer.push(item, distance);
+							}
+						}
+					}
+				}
+			}
+			break;
+	}
+	if(buffer.length() == 0){
+		buffer.push(oldPositions.concat([0, 0, 0, 0]).concat(commonData), 0);
+		return buffer.toArray();		
+	} else {
+		return buffer.toArray();
+	}	
 }
 
 function limitedSearch(song, arm_positions, startIndex, limit, branching){
@@ -251,7 +359,7 @@ function limitedSearch(song, arm_positions, startIndex, limit, branching){
 		if(songIndex - startIndex >= limit){
 			return current;
 		}
-		var nexts = multiMinCostNextState(current[current.length - 1].slice(0, 4), song[songIndex], songIndex + 1, branching);
+		var nexts = betterMultiMinCostNextState(current[current.length - 1].slice(0, 4), song[songIndex], songIndex + 1, branching);
 		for(var i = 0; i < nexts.length;i++){
 			var next = current.slice(0);
 			next.push(nexts[i][0].concat(nexts[i][1]));
@@ -263,6 +371,7 @@ function limitedSearch(song, arm_positions, startIndex, limit, branching){
 
 function limitedSearchPlanner(song, arm_positions){
 	var plan = [[arm_positions[0], arm_positions[1], arm_positions[2], arm_positions[3], 0, 0, 0, 0, 0]];
+	var t = new Date();
 	for(var i = 0; i < song.length; i++){
 		var next = limitedSearch(song, plan[plan.length - 1].slice(0, 4), i, Math.min(searchDepth, song.length - i), branching);
 		if(next != null){
@@ -270,6 +379,7 @@ function limitedSearchPlanner(song, arm_positions){
 			plan.push(next);
 		}
 	}
+	//console.log((new Date()) - t);
 	return plan;
 }
 
